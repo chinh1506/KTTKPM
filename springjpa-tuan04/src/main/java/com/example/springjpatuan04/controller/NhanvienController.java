6c8256c6a44766e6d99b4a00790b547bc8ee2eba
Hoang Van Chinh <Chinhhoang115@gmail.com>
2022-09-19 08:14:40 +0700

    cau3

--- springjpa-tuan04/src/main/java/com/example/springjpatuan04/controller/NhanvienController.java
+++ springjpa-tuan04/src/main/java/com/example/springjpatuan04/controller/NhanvienController.java
@@ -1,14 +1,20 @@
 package com.example.springjpatuan04.controller;
 
+import com.example.springjpatuan04.model.Nhanvien;
 import com.example.springjpatuan04.repository.NhanvienRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/nhanvien")
 public class NhanvienController {
     @Autowired
     private NhanvienRepository nhanvienRepository;
-
+    @GetMapping("cau3")
+    public List<Nhanvien> cau3(){
+        return nhanvienRepository.findByLuongLessThanEqual(10000);
+    }
 }
